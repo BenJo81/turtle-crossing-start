8fd8119bf8c4f204b17f986472886f8f75b7bb8a
Ben Joseph <bjoseph@sourceallies.com>
2023-12-02 16:10:07 -0600

    finished game

--- car_manager.py
+++ car_manager.py
@@ -6,14 +6,26 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
 
     def __init__(self):
-        super().__init__()
-        self.color(random.choice(COLORS))
-        self.shape("square")
-        self.turtlesize(stretch_wid=2)
-        self.penup()
-        self.setheading(180)
-        self.goto(290, random.randint(-250, 250))
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.color(random.choice(COLORS))
+            new_car.shape("square")
+            new_car.shapesize(stretch_len=2)
+            new_car.penup()
+            new_car.goto(300, random.randint(-250, 250))
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,22 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 250)
+        self.score = 1
+        self.write(f"Score: {self.score}", align='left', font=FONT)
+
+    def score_increase(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Level: {self.score}", align='left', font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align='center', font=FONT)
